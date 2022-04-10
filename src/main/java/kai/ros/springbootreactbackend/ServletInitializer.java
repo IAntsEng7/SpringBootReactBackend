e87310de92bd9d90094929da1368a10cd4167036
IAntsEng7 <83633994+IAntsEng7@users.noreply.github.com>
2022-04-10 18:29:03 +0800

    Initial commit

---
+++ src/main/java/kai/ros/springbootreactbackend/ServletInitializer.java
@@ -0,0 +1,13 @@
+package kai.ros.springbootreactbackend;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(SpringBootReactBackendApplication.class);
+  }
+
+}
---
+++ src/test/java/kai/ros/springbootreactbackend/SpringBootReactBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kai.ros.springbootreactbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
