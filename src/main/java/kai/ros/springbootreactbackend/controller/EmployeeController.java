863048095b9f051f4b9e9458ea3bda86f35e9d4b
IAntsEng7 <83633994+IAntsEng7@users.noreply.github.com>
2022-04-10 21:19:15 +0800

    new model, Repo, controller; edit application.properties

---
+++ src/main/java/kai/ros/springbootreactbackend/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package kai.ros.springbootreactbackend.controller;
+
+import java.util.List;
+import kai.ros.springbootreactbackend.model.Employee;
+import kai.ros.springbootreactbackend.repository.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+  @Autowired
+  private EmployeeRepo employeeRepo;
+
+  // get all employee
+  @GetMapping("/employees")
+  public List<Employee> getAllEmployee(){
+    return employeeRepo.findAll();
+  }
+
+
+}
---
+++ src/main/java/kai/ros/springbootreactbackend/repository/EmployeeRepo.java
@@ -0,0 +1,10 @@
+package kai.ros.springbootreactbackend.repository;
+
+import kai.ros.springbootreactbackend.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+
+}
