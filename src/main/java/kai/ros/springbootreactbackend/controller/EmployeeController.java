e1fd2372d50eeda7e0bc08047c9fdca9263a08ad
IAntsEng7 <83633994+IAntsEng7@users.noreply.github.com>
2022-05-15 09:49:19 +0800

    update fixed

--- src/main/java/kai/ros/springbootreactbackend/controller/EmployeeController.java
+++ src/main/java/kai/ros/springbootreactbackend/controller/EmployeeController.java
@@ -1,12 +1,15 @@
 package kai.ros.springbootreactbackend.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import kai.ros.springbootreactbackend.exception.ResourceNotFoundException;
 import kai.ros.springbootreactbackend.model.Employee;
 import kai.ros.springbootreactbackend.repository.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,12 +54,21 @@ public class EmployeeController {
         .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id : " + id));
 
     employee.setFirstName(employeeDetails.getFirstName());
-    employee.setLastName(employeeDetails.getFirstName());
+    employee.setLastName(employeeDetails.getLastName());
     employee.setEmailId(employeeDetails.getEmailId());
     Employee updateEmployee = employeeRepo.save(employee);
 
     return ResponseEntity.ok(updateEmployee);
   }
 
-
+  // delete employee rest api
+  @DeleteMapping("/employees/{id}")
+  public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
+    Employee employee = employeeRepo.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id : " + id));
+    employeeRepo.delete(employee);
+    Map<String, Boolean> response = new HashMap<>();
+    response.put("delete", Boolean.TRUE);
+    return ResponseEntity.ok(response);
+  }
 }
